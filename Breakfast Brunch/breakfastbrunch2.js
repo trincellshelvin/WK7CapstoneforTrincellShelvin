961e8e504e8bcff01b9d7f8ae11e14959aea11a1
Trincell Shelvin <trincellshelvin@gmail.com>
2024-07-13 21:53:01 -0500

    update

--- Breakfast Brunch/breakfastbrunch2.js
+++ Breakfast Brunch/breakfastbrunch2.js
@@ -1,3 +1,9 @@
-function {
-    document.write 
+function submitOrder {
+    let order = {}
+    order.patron = prompt("What is the name for this order?");
+    order.number = prompt("What is the phone number for this order?");
+    order.eggs = prompt("What eggs would you like and cooked how? *Can have 2");
+    order.protien = prompt("What protien would you like? *Note, for Steak and Lamb, please advised of cook. Can add up to 2 for $3 extra");
+    
 }
+    sendordertoKitchen.onclick = submitOrder
