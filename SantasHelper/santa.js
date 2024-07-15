5ea831e14d7ab01e1836e7c54783ecf2ed94fd4d
Trincell Shelvin <trincellshelvin@gmail.com>
2024-07-15 08:43:16 -0500

    update

--- SantasHelper/santa.js
+++ SantasHelper/santa.js
@@ -6,17 +6,17 @@ function createMessage() {
   santasParagraph.innerHTML = `Dear Santa, Hi this is ${message.sender}, I just want you to know that I have been very good this year. I want to request a special wish from you this year. I would love to have ${message.request} as my gift this year. Sincerely, ${message.sender}`;
 
 }
-sendmessagetoSanta.onclick = createMessage; 
+sendmessagetoSanta.onclick = createMessage;
 
 
 function printpage() {
   window.print();
-  
+
 }
 
 let santaletter = document.getElementById("santaletter");
-santaletter.addEventListener("click", 
-function () {
-  document.body.style.backgroundColor.backgroundImage =  "url(https://images.pexels.com/photos/6057134/pexels-photo-6057134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
+santaletter.addEventListener("click",
+  function () {
+    document.body.style.backgroundColor.backgroundImage = "url(https://images.pexels.com/photos/6057134/pexels-photo-6057134.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1)";
 
-})
+  })
