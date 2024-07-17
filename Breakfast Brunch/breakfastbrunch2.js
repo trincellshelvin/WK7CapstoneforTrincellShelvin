0031a10e998d3d1ed19175ba8449c67d50cc3853
Trincell Shelvin <trincellshelvin@gmail.com>
2024-07-17 15:49:34 -0500

    update

--- Breakfast Brunch/breakfastbrunch2.js
+++ Breakfast Brunch/breakfastbrunch2.js
@@ -5,30 +5,19 @@ function submitorder() {
     order.patron = prompt("What is the name for this order?");
     order.number = prompt("What is the phone number for this order?");
     order.eggs = prompt("What eggs would you like and cooked how?");
-    order.protein = prompt("What protein would you like? *Note, for Steak and Lamb, please advised of cook. One comes with meal can add one more for $3 extra");
+    order.protein = prompt("What protein would you like? *Note, for Steak and Lamb, please advised of cook.");
     order.sides = prompt("What two sides would you like?");
-    order.drinks = prompt("What drink would you like with our meal? Can pick 2 with an extra $2 for the extra drink.");
+    order.drinks = prompt("What drink would you like with our meal?");
     order.condiments = prompt("What condiments would you like *coffee already comes with 2 cream and 3 sugars?");
-    order.extra = prompt("Any extra menu items? *extra charge for each one requested.");
 
     orderParagraph.innerHTML = `Thanks for your order! Your order total is ${mealtotal}. Please have correct change or expect to pay with card upon order pick up. We accept Cashapp payment to $themeetingplace as well upon arrival.`;
 }
 submitOrderButton.onclick = submitorder
 
+
 function mealtotal(){
-    let mealsubtotal = 17
-    let extrasides = 2 
-    let extradrinks = 2
-    let extraprotein = 3
-    let baconhamsausage = 1.50
-    let lamb = 4
-    let salmon = 2
-    let i = [0]
-    if (mealsubtotal === 17) text = 'Your total is $17';
-    if (order.extradrinks)  add = mealsubtotal + extradrinks(i);
-    if (order.extrasides) add = mealsubtotal + extrasides(i);
-    if (order.extraprotein) add = mealsubtotal + extraprotein(i);
-
+    let mealsubtotal = 24;
+    let taxes = .10;
+    mealtotal = mealsubtotal * taxes;
+    
 }
-
-
